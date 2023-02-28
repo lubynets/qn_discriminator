aa78ca24880ac76f06b44f1338cb13892c569471
Oleksii Lubynets <lubinets95@gmail.com>
2023-02-28 15:39:04 +0100

    clean code

--- GraphExtractor.cpp
+++ GraphExtractor.cpp
@@ -2,32 +2,7 @@
 
 void GraphExtractor::SetSelectAxis(std::string select_axis) {
   select_axis_ = select_axis;
-
-  //   number_of_select_axis_ = 0;
-  //   while(data_container_->GetAxes().at(number_of_select_axis_).Name() != select_axis_)
-  //     number_of_select_axis_++;
 }
-
-// TGraph* GraphExtractor::GetGraph(std::vector<int> bin)
-// {
-//   if(bin.size() != names_axes_to_exclude_.size())
-//     throw std::runtime_error("Bin vector's dimensionality must equal to number of axes to exclude");
-//
-//   std::vector<Qn::DataContainer<Qn::StatCalculate,Qn::Axis<double>>> data_container_reduced;
-//   data_container_reduced.push_back(*data_container_);
-//
-//   for(int j=0; j<bin.size(); j++)
-//   {
-//     double left_edge = data_container_ -> GetAxis(names_axes_to_exclude_.at(j).c_str()).GetLowerBinEdge(bin.at(j));
-//     double right_edge = data_container_ -> GetAxis(names_axes_to_exclude_.at(j).c_str()).GetLowerBinEdge(bin.at(j)+1);
-//     Qn::Axis<double> axis(names_axes_to_exclude_.at(j), 1, left_edge, right_edge);
-//     data_container_reduced.push_back(data_container_reduced.back().Select(axis));
-//   }
-//
-//   TGraph* graph = Qn::DataContainerHelper::ToTGraph(data_container_reduced.back());
-//
-//   return graph;
-// }
 
 TGraphErrors* GraphExtractor::GetGraph(std::vector<size_t> bin) const {
   if (data_container_->GetAxes().size() - 1 != bin.size())
@@ -52,26 +27,6 @@ TGraphErrors* GraphExtractor::GetGraph(std::vector<size_t> bin) const {
   return graph;
 }
 
-// TGraphErrors* GraphExtractor::GetGraph(std::vector<size_t> bin) const
-// {
-//   const auto& axis = data_container_->GetAxis(select_axis_.c_str());
-//   TGraphErrors* graph = new TGraphErrors();
-//
-//   for(int iselect=0; iselect<axis.size(); iselect++) {
-//     std::vector<size_t> bin4D = bin;
-//     bin4D.insert(bin4D.begin()+number_of_select_axis_, iselect);
-//     Qn::StatCalculate scalc = data_container_->At(bin4D);
-//     const double x = axis.GetBinCenter(iselect);
-//     const double y = scalc.Mean();
-//     const double ey = scalc.StandardErrorOfMean();
-//     graph->SetPoint(iselect, x, y);
-//     graph->SetPointError(iselect, 0, ey);
-//     graph->GetXaxis()->SetTitle(select_axis_.c_str());
-//   }
-//
-//   return graph;
-// }
-
 std::vector<int> GraphExtractor::GetAxesSizes() {
   std::vector<int> sizes;
   for (auto& axisname : names_axes_to_exclude_)
@@ -91,3 +46,4 @@ std::vector<std::vector<double>> GraphExtractor::GetAxesBinEdges() {
 
   return v_binedges;
 }
+
--- GraphExtractor.hpp
+++ GraphExtractor.hpp
@@ -13,7 +13,7 @@ class GraphExtractor {
   GraphExtractor() = default;
   virtual ~GraphExtractor() = default;
 
-  void SetDataContainer(Qn::DataContainer<Qn::StatCalculate, Qn::Axis<double>>* datacontainer) { data_container_ = (Qn::DataContainer<Qn::StatCalculate, Qn::Axis<double>>*) datacontainer; };
+  void SetDataContainer(Qn::DataContainer<Qn::StatCalculate, Qn::Axis<double>>* datacontainer) { data_container_ = datacontainer; };
   void SetNamesAxesToExclude(std::vector<std::string> names_axes_to_exclude) { names_axes_to_exclude_ = names_axes_to_exclude; };
   void SetSelectAxis(std::string select_axis);
 
